84b7160bc160c1ea30c55e973913e492d940dc4e
teguhsbl <teguhsabila4@gmail.com>
2024-09-11 00:14:20 +0700

    third commit

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,17 +7,15 @@ export default {
     logo: "/me.jpg",
     siteTitle: "Rokuro Inosuke",
     nav: [
-    { text: "Project", link: "/project/"},
-    { text: "Blog", link: "/blog/"},
+    { text: "Blog", link: "//blog.rokuro.my.id"},
     { text: "About", link: "/about" },
     { text: "Contact", link: "/contact" },
     ],
     socialLinks: [
     { icon: "github", link: "https://github.com/teguhsbl" },
-    { icon: "facebook", link: "https://fb.me/sabilateguh" },
     ],
     footer: {
-   message: "This Portofolio build with Vitepress.",
+   message: "This is my personal website, which combines various of my platforms.",
    copyright: "Copyright © 2024-present Rokuro Inosuke",
  },
   },
